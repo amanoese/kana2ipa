87d8221f75f421d3d052cdb84ace7a335e734233
amanoese <amanoese@gmail.com>
2019-12-23 22:40:50 +0900

    add feature hiragana

--- __tests__/kana2ipa.js
+++ __tests__/kana2ipa.js
@@ -10,6 +10,15 @@ describe('kana2ipa',()=>{
     expect(converter.kana2ipa('シンゴウ')).toBe('ɕiŋɡoː') //ɕiŋːoːがただしい？
     expect(converter.kana2ipa('ケンイチ')).toBe('keẽit͡ɕi')
   })
+  test('ひらがなでも変換できることの確認',async ()=>{
+    expect(converter.kana2ipa('かんぱん')).toBe('kampaɴ')
+    expect(converter.kana2ipa('てんまど')).toBe('temːado')
+    expect(converter.kana2ipa('ぼんたん')).toBe('bontaɴ')
+    expect(converter.kana2ipa('はんのう')).toBe('hanːoː')
+    expect(converter.kana2ipa('びんかん')).toBe('bʲiŋkaɴ') // ビの発音はbʲi̞
+    expect(converter.kana2ipa('しんごう')).toBe('ɕiŋɡoː') //ɕiŋːoːがただしい？
+    expect(converter.kana2ipa('けんいち')).toBe('keẽit͡ɕi')
+  })
 })
 
 
--- src/converter.js
+++ src/converter.js
@@ -1,15 +1,23 @@
+const XRegExp = require('xregexp');
+XRegExp.install('astral');
+
 let ipa = require('./ipa')
 let ipa_small = require('./ipa-small')
 
 module.exports = {
+  hiragana2katakana(text) {
+    const _regexp = XRegExp('\\p{Hiragana}','g')
+    return text.replace(_regexp,v=> String.fromCharCode(+v.charCodeAt(0) + 0x60))
+  },
   kana2ipa(inText) {
     if (inText.length <= 0) {
       return inText
     }
     let ipa_small_match = Object.keys(ipa_small).join('|').replace(/.*/,'($&)')
 
+    let katakanaText = this.hiragana2katakana(inText)
     // replace Basic Word
-    let bIpaString = inText
+    let bIpaString = katakanaText
       .normalize('NFKC') // harf size to wide size
       .replace(new RegExp(ipa_small_match,'g'),v=>(ipa_small[v]||v))
       .match(/./ug)
